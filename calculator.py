5d8954e854208e4d46d94c2511025ad8b8f62b41
kingmakerkor <peter31jjang@gmamil.com>
2024-10-28 18:42:32 +0900

    Add get_Abs function

--- calculator.py
+++ calculator.py
@@ -12,7 +12,13 @@ def divide_new(a, b)
   return a/b
 
 def get_Median(a, b):
-    return (a+b)/2
+  return (a+b)/2
 
 def get_Remainder(a, b):
-    return a//b
+  return a//b
+
+def get_Abs(num):
+  if num>=0:
+     return num
+  else:
+     return -num
